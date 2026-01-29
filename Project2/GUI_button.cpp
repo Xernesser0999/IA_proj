dffc1e5d12257768c3317fdac5f06e636e5619a2
Xern <steverpicx@gmail.com>
2026-01-29 09:45:16 +0100

    button

--- Project2/GUI_button.cpp
+++ Project2/GUI_button.cpp
@@ -1,1 +1,25 @@
 #include "GUI_button.h"
+
+GUI_button::GUI_button(float posX_, float posY_, std::string file) {
+
+}
+
+GUI_button::~GUI_button() {
+	TX = nullptr;
+	sprite = nullptr;
+
+	delete TX;
+	delete sprite;
+}
+
+bool GUI_button::hovered() {
+
+}
+
+bool GUI_button::clicked() {
+
+}
+
+void GUI_button::render(sf::RenderWindow& window) {
+	window.draw(*sprite);
+}
--- Project2/GUI_button.h
+++ Project2/GUI_button.h
@@ -1,4 +1,23 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <optional>
+
+#include <string>
+
 class GUI_button {
+	float posX;
+	float posY;
+	sf::Vector2f pos;
+
+	sf::Texture* TX;
+	sf::Sprite* sprite;
+
+	GUI_button(float posX_, float posY_, std::string file);
+	~GUI_button();
+
+	void render(sf::RenderWindow& window);
+
+	bool hovered();
+	bool clicked();
 };
 
