0bcb3b9e0d53aef9662e96c1a7787d057ed7be53
Yuna <lharispe@gaming.tech>
2026-01-31 18:36:14 +0100

    moveTo working

--- Project2/Npc.cpp
+++ Project2/Npc.cpp
@@ -1,30 +1,53 @@
 #include "Npc.h"
 
-Npc::Npc(){
-
+Npc::Npc() {
 }
 
 Npc::Npc(float posX_, float posY_, float sizeX, float sizeY, float speed_, std::string file) : Pawn(posX_, posY_, sizeX, sizeY, speed_, file) {
-
 }
 
 void Npc::update(float dt, Bakery* shop) {
 	shop_ = shop;
-	rectangle->setPosition(pos);
-	while (!arrived) {
-		
-		this->moveTo(dt, shop_);
+
+	if (!arrived) {
+		moveTo(dt, shop_);
 	}
+
+	pos = { posX, posY };
+	rectangle->setPosition(pos);
 }
 
-void Npc::moveTo(float dt, Bakery* shop){
-	if (posX <= shop->pos.x) {
-		posX += 200;
-	}
-	else if  (posX == shop -> pos.x && posY <= shop->pos.y) {
-		posY += speed * dt;
-	}
-	else {
+void Npc::moveTo(float dt, Bakery* shop) {
+	float targetX = shop->pos.x + (shop->size.x/2);
+	float targetY = shop->pos.y + shop->size.y;
+
+	if (posX == targetX && posY == targetY) {
 		arrived = true;
+		return;
+	}
+
+	if (posX < targetX) {
+		posX += speed * dt;
+		if (posX > targetX) {
+			posX = targetX;
+		}
+	}
+	else if (posX > targetX) {
+		posX -= speed * dt;
+		if (posX < targetX) { 
+			posX = targetX;
+		}
+	}
+	else if (posY < targetY) {
+		posY += speed * dt;
+		if (posY > targetY) {
+			posY = targetY;
+		}
+	}
+	else if (posY > targetY) {
+		posY -= speed * dt;
+		if (posY < targetY) {
+			posY = targetY;
+		}
 	}
 }
